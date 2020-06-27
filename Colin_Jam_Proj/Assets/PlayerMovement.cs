b591b33c115bc5bc49f79cabfaef3f3b07ac87d5
Jameson Danning <jdanning@ucsc.edu>
2020-06-27 00:05:27 -0700

    Birb movement all good now

--- Colin_Jam_Proj/Assets/PlayerMovement.cs
+++ Colin_Jam_Proj/Assets/PlayerMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb2d;
-    public float force;
-    public float maxSpeed;
+    public float speed;
+    public GameObject self;
+    //private float curSpeed;
+    //private float maxSpeed;
+    public float maxVelocity;
 
     float horizontal;
     float vertical;
@@ -16,20 +19,38 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = gameObject.AddComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
        
 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + force * horizontal;
-        position.y = position.y + force * vertical;
-        transform.position = position;
+        Vector2 AxisInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+
+        rb2d.AddForce(AxisInput * speed, ForceMode2D.Impulse); 
+
+        //limits velocity
+        rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxVelocity);
+
+        //slows bird down when no input
+        if (AxisInput == Vector2.zero)
+        {            
+            rb2d.velocity /= new Vector2(1.05f, 1.05f);         
+        }
+
+        if(AxisInput.x > 0)
+        {
+            self.transform.localScale = new Vector2(-4f, 4f);
+        }
+        else if(AxisInput.x < 0)
+        {
+            self.transform.localScale = new Vector2(4f, 4f);
+        }
+
+
     }
 }
