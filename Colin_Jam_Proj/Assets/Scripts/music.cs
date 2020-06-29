c3162d10a0c28ffe5cfa5b5d9b3087410d1c1109
Jacob Compton <jrcompto@ucsc.edu>
2020-06-28 22:03:47 -0700

    Added Background music and sfx

---
+++ Colin_Jam_Proj/Assets/Scripts/music.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour
+{
+    private static music instance = null;
+    public static music Instance
+    {
+        get { return instance; }
+    }
+    //private AudioSource _audioSource;
+    //bool startedAudio = false;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+        //_audioSource = GetComponent<AudioSource>();
+    }
+
+    /*private void Start()
+    {
+        if (!startedAudio)
+        {
+            PlayMusic();
+            startedAudio = true;
+        }
+    }*/
+
+    /*public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }*/
+}
